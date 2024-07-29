ea8790472766b0f8dcba19d411403baba7329f3a
lordkintaru <milinim4s@gmail.com>
2024-07-29 08:14:35 -0300

    Realizando as alterações do index para o que foi proposto

--- dist/index.js
+++ dist/index.js
@@ -1,44 +1,23 @@
 "use strict";
+/*Criar um vetor de Funcionários e insira nele alguns Gerentes e Atendentes (pelo menos quatro de cada, de forma intercalada e com valores de atributos diversificados). Em seguida, usar a estrutura forEach no vetor para executar o método toString dos funcionários, a fim de testar o comportamento polimórfico daquele método.
+Usar o método bonificacao() em objetos gerente e atendente, sem informar o valor do percentual como parâmetro, e verificar se os cálculos foram realizados conforme especificado para cada classe.
+Usar o método bonificacao(percentual) em objetos gerente e atendente, informando o valor do percentual como parâmetro, e verificar se os cálculos foram realizados conforme especificado para cada classe.*/
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const Pessoa_1 = __importDefault(require("./Pessoa"));
 const Atendente_1 = __importDefault(require("./Atendente"));
 const Gerente_1 = __importDefault(require("./Gerente"));
-const Funcionario_1 = __importDefault(require("./Funcionario"));
-// Criando instâncias de Pessoa
-const pessoa1 = new Pessoa_1.default("Alexandre", "Rua dos Bobos, 0", "11999999999", new Date(2000, 0, 1));
-const pessoa2 = new Pessoa_1.default("Igor", "Rua dos Bobos, 1", "11999999998", new Date(2000, 1, 1));
-const pessoa3 = new Pessoa_1.default("Pedro", "Rua dos Bobos, 2", "11999999997", new Date(2000, 2, 1));
-const pessoa4 = new Pessoa_1.default("Maria", "Rua dos Bobos, 3", "11999999996", new Date(2000, 3, 1));
-const pessoa5 = new Pessoa_1.default("João", "Rua dos Bobos, 4", "11999999995", new Date(2000, 4, 1));
-// Exibindo informações das pessoas
-console.log(pessoa1.toString());
-console.log(pessoa2.toString());
-console.log(pessoa3.toString());
-console.log(pessoa4.toString());
-console.log(pessoa5.toString());
-// Criando instâncias de Funcionário
-const funcionario1 = new Funcionario_1.default(3000, "Agência Central", "Carlos", "Rua Principal, 123", "11988888888", new Date(1995, 5, 15));
-const funcionario2 = new Funcionario_1.default(3200, "Agência Norte", "Roberta", "Rua Secundária, 456", "11977777777", new Date(1985, 10, 25));
-// Exibindo informações dos funcionários
-console.log(funcionario1.toString());
-console.log(funcionario2.toString());
-// Criando instâncias de Atendente
-const atendente1 = new Atendente_1.default("caixa", 2500, "Agência 1", "Fernando", "Rua A, 10", "11966666666", new Date(1990, 3, 10));
-const atendente2 = new Atendente_1.default("pessoal", 2700, "Agência 2", "Ana", "Rua B, 20", "11955555555", new Date(1987, 7, 22));
-// Exibindo informações dos atendentes
-console.log(atendente1.toString());
-console.log(`Bonificação: ${atendente1.bonificacao()} reais`);
-console.log(atendente2.toString());
-console.log(`Bonificação: ${atendente2.bonificacao()} reais`);
-// Criando instâncias de Gerente
-const gerente1 = new Gerente_1.default("regional", 5000, "Agência 1", "Marta", "Rua C, 30", "11944444444", new Date(1980, 1, 5));
-const gerente2 = new Gerente_1.default("conta", 4500, "Agência 2", "Lucas", "Rua D, 40", "11933333333", new Date(1978, 9, 15));
-// Exibindo informações dos gerentes
-console.log(gerente1.toString());
-console.log(`Bonificação: ${gerente1.bonificacao()} reais`);
-console.log(gerente2.toString());
-console.log(`Bonificação: ${gerente2.bonificacao()} reais`);
+const vetFuncionarios = [
+    new Atendente_1.default("caixa", 2500, "Agência 1", "Fernando", "Rua A, 10", "11966666666", new Date(1990, 3, 10)),
+    new Gerente_1.default("regional", 5000, "Agência 1", "Marta", "Rua C, 30", "11944444444", new Date(1980, 1, 5)),
+    new Atendente_1.default("pessoal", 2700, "Agência 2", "Ana", "Rua B, 20", "11955555555", new Date(1987, 7, 22)),
+    new Gerente_1.default("regional", 6000, "Agência 2", "Carlos", "Rua D, 40", "11933333333", new Date(1975, 5, 15)),
+    new Atendente_1.default("caixa", 2800, "Agência 3", "Pedro", "Rua E, 50", "11944444444", new Date(1995, 11, 5)),
+    new Gerente_1.default("conta", 4500, "Agência 3", "Lucas", "Rua F, 60", "11933333333", new Date(1978, 9, 15)),
+    new Atendente_1.default("pessoal", 2900, "Agência 4", "Juliana", "Rua G, 70", "11922222222", new Date(2000, 1, 1)),
+    new Gerente_1.default("conta", 5500, "Agência 4", "Bruno", "Rua H, 80", "11911111111", new Date(1985, 3, 3))
+]; // Vetor para armazenar os atendentes e gerentes
+//
+vetFuncionarios.forEach((funcionario) => console.log(`Bonificação do ${funcionario.nome}: ${funcionario.bonificacao()}`));
 //# sourceMappingURL=index.js.map
